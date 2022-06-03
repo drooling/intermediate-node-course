a9a8928ca2c31a8a04fe1090c2eaf1572049aa32
drooling <97707885+drooling@users.noreply.github.com>
2022-06-03 13:46:37 -0500

    Added create and find user routes

--- server.js
+++ server.js
@@ -12,13 +12,52 @@ app.listen(80, () => {
 })
 
 app.post('/users', (req, res) => {
-	// User.create()
+	User.create({
+		name: req.body.newUser.name,
+		email: req.body.newUser.email,
+		password: req.body.newUser.password
+	}, (err, data) => {
+		if (err) {
+			res.json({
+				success: false,
+				message: err
+			})
+		} else if (!data) {
+			res.json({
+				success: false,
+				message: "Not Found"
+		})
+		} else {
+			res.json({
+				success: true,
+				message: data
+			})
+		}
+	})
 })
+
 
 app.route('/users/:id')
 
 .get((req, res) => {
-	// User.findById()
+	User.findById(req.params.id, (err, data) => {
+		if (err) {
+			res.json({
+				success: false,
+				message: err
+			})
+		} else if (!data) {
+			res.json({
+				success: false,
+				message: "Not found"
+			})
+		} else {
+			res.json({
+				success: true,
+				message: data
+			})
+		}
+	})
 })
 
 .put((req, res) => {
